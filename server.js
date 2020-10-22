22ab28afdd9421c528215ab11d9b3945b2879210
Michael Giddings <mikegiddings@gmail.com>
2020-10-22 15:19:45 -0400

    add post route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const express = require('express');
 const PORT = process.env.PORT || 3001;
@@ -99,6 +100,29 @@ app.delete('/api/candidate/:id', (req, res) => {
         });
     });
 });  
+
+/* POST */
+/* INSERT new row */
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body,'first_name', 'last_name', 'industry_connected');
+    if(errors){
+        res.status(400).json({ error: errors });
+    }
+    const sql = `INSERT INTO candidates(first_name, last_name, industry_connected)
+                VALUES(?,?,?)`;
+    const params =[body.first_name,body.last_name,body.industry_connected];
+    db.run(sql, params, function(err,result){
+        if(err){
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            body: body,
+            id: this.lastID
+        });
+    });
+});
 
 
 app.use((req,res)=>{
